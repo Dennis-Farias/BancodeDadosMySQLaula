b2797eb5a2812c2db7db16ffa6431995861ca320
Dennis de Sousa Farias <101424784+Dennis-Farias@users.noreply.github.com>
2023-05-29 01:09:39 -0300

    Atividades

---
+++ Atividades, arquivo 5.sql
@@ -0,0 +1,37 @@
+START TRANSACTION; 
+	DELETE FROM administrador; 
+    INSERT INTO administrador(adm_nome, adm_cpf, adm_e_mail, adm_senha) 
+		VALUES ('Dennis de Sousa Farias', '01234567890', 'dennisfarias@example.com', 'senha123');
+	SELECT * FROM administrador;
+ROLLBACK;
+
+START TRANSACTION; 
+	UPDATE administrador SET adm_senha = 'senha123' WHERE adm_id = 10;
+	SELECT * FROM administrador;
+ROLLBACK;
+
+select * from formas_de_pagamento;
+
+select prd_ped_valor from produtos_pedidos;
+
+select distinct end_estado from endereco;
+
+select prd_nome, prd_preco from produtos where prd_preco > 50 order by prd_preco;
+
+select prd_nome, prd_estoque from produtos where prd_estoque between 20 and 50 order by prd_estoque;
+
+select cli_nome from clientes where cli_nome like '%Santos';
+
+select cli_nome from clientes where cli_nome like 'a_a%';
+
+select ped_id, ent_status from entrega where ent_status in ('Pendente', 'Em andamento');
+
+select ped_id, fg_id from pedidos where fg_id = 1 or fg_id = 2;
+
+select end_cidade from endereco where not end_cidade = 'Rio de Janeiro' group by end_cidade;
+
+select cli_nome from clientes where cli_nome like 'a_a%';
+
+select concat("O pedido ", ped_id, " do cliente ", cli_id, " está ", ent_status) as 'Situação dos pedidos' from entrega where cli_id >= 3;
+
+select cli_nome from clientes limit 0, 10;
--- Inserindo dados nas tabelas, arquivo 4.sql
+++ Inserindo dados nas tabelas, arquivo 4.sql
@@ -11,16 +11,26 @@ VALUES ('Camiseta', 'Camiseta de algodão', 50, 'M', 29.99),
        ('Bolsa', 'Bolsa de couro', 8, 'Único', 69.99);
        
 INSERT INTO clientes (cli_nome, cli_telefone, cli_e_mail, cli_senha, cli_cpf)
-VALUES ('João Silva', '123456789', 'joao.silva@example.com', 'senha123', '12345678901'),
-       ('Maria Santos', '987654321', 'maria.santos@example.com', 'senha456', '98765432102'),
-       ('Pedro Oliveira', '111222333', 'pedro.oliveira@example.com', 'senha789', '11122233303'),
-       ('Ana Pereira', '999888777', 'ana.pereira@example.com', 'senhaabc', '99988877704'),
-       ('Carlos Souza', '444555666', 'carlos.souza@example.com', 'senhadef', '44455566605'),
-       ('Laura Almeida', '777888999', 'laura.almeida@example.com', 'senhaghi', '77788899906'),
-       ('Felipe Castro', '222333444', 'felipe.castro@example.com', 'senhajkl', '22233344407'),
-       ('Sofia Lima', '666777888', 'sofia.lima@example.com', 'senhamno', '66677788808'),
-       ('Gabriel Torres', '888999000', 'gabriel.torres@example.com', 'senhapqr', '88899900009'),
-       ('Luisa Fernandes', '555444333', 'luisa.fernandes@example.com', 'senhastu', '55544433310');
+VALUES ('Maria Silva', '11987654321', 'maria.silva@example.com', 'senha123', '12345678901'),
+       ('João Santos', '11912345678', 'joao.santos@example.com', 'senha456', '98765432109'),
+       ('Ana Oliveira', '11923456789', 'ana.oliveira@example.com', 'senha789', '45678912345'),
+       ('Pedro Souza', '11934567890', 'pedro.souza@example.com', 'senhaabc', '65432198709'),
+       ('Sara Lima', '11945678901', 'sara.lima@example.com', 'senhaxyz', '32198765409'),
+       ('Carlos Pereira', '11956789012', 'carlos.pereira@example.com', 'senha123', '78912345609'),
+       ('Fernanda Santos', '11967890123', 'fernanda.santos@example.com', 'senha456', '98765432101'),
+       ('Lucas Oliveira', '11978901234', 'lucas.oliveira@example.com', 'senha789', '45678912309'),
+       ('Mariana Costa', '11989012345', 'mariana.costa@example.com', 'senhaabc', '65432198745'),
+       ('Rafaela Fernandes', '11900123456', 'rafaela.fernandes@example.com', 'senhaxyz', '32198765445'),
+       ('Roberto Almeida', '11911223344', 'roberto.almeida@example.com', 'senha123', '11122233344'),
+       ('Laura Santos', '11922334455', 'laura.santos@example.com', 'senha456', '22233344455'),
+       ('Diego Oliveira', '11933445566', 'diego.oliveira@example.com', 'senha789', '33344455566'),
+       ('Luana Souza', '11944556677', 'amanda.souza@example.com', 'senhaabc', '44455566677'),
+       ('Pedro Henrique Lima', '11955667788', 'pedrohenrique.lima@example.com', 'senhaxyz', '55566677788'),
+       ('Carolina Costa', '11966778899', 'carolina.costa@example.com', 'senha123', '66677788999'),
+       ('Gabriel Fernandes', '11977889900', 'gabriel.fernandes@example.com', 'senha456', '77788990000'),
+       ('Aline Oliveira', '11988990011', 'aline.oliveira@example.com', 'senha789', '88899001111'),
+       ('Ricardo Silva', '11990000123', 'ricardo.silva@example.com', 'senhaabc', '99900001234'),
+       ('Bruna Santos', '11901111234', 'bruna.santos@example.com', 'senha123', '11111122345');
        
 INSERT INTO administrador (adm_nome, adm_cpf, adm_e_mail, adm_senha)
 VALUES ('João Silva', '12345678901', 'joao.silva@example.com', 'senha123'),
@@ -71,27 +81,35 @@ VALUES (1, 1, 2, 50.00),
        (10, 5, 1, 40.00);
        
 INSERT INTO entrega (ped_id, cli_id, ent_data, ent_status, end_id)
-VALUES (1, 1, '2023-05-01', 'Pendente', 1),
-       (2, 2, '2023-05-02', 'Em andamento', 2),
-       (3, 3, '2023-05-03', 'Concluída', 3),
-       (4, 4, '2023-05-04', 'Pendente', 4),
-       (5, 5, '2023-05-05', 'Em andamento', 5),
-       (6, 1, '2023-05-06', 'Concluída', 1),
-       (7, 2, '2023-05-07', 'Pendente', 2),
-       (8, 3, '2023-05-08', 'Em andamento', 3),
-       (9, 4, '2023-05-09', 'Concluída', 4),
-       (10, 5, '2023-05-10', 'Pendente', 5);
-
+VALUES (1, 1, '2022-03-10', 'Pendente', 1),
+       (2, 2, '2022-06-20', 'Em andamento', 2),
+       (3, 3, '2022-09-15', 'Concluída', 3),
+       (4, 4, '2023-02-28', 'Pendente', 4),
+       (5, 5, '2023-05-12', 'Em andamento', 5),
+       (6, 1, '2023-08-05', 'Concluída', 1),
+       (7, 2, '2023-11-21', 'Pendente', 2),
+       (8, 3, '2024-04-03', 'Em andamento', 3),
+       (9, 4, '2024-07-18', 'Concluída', 4),
+       (10, 5, '2024-10-30', 'Pendente', 5);
 
 INSERT INTO endereco (cli_id, end_cidade, end_estado, end_bairro, end_num, end_logradouro)
 VALUES (1, 'São Paulo', 'SP', 'Centro', '123', 'Rua A'),
-       (1, 'Rio de Janeiro', 'RJ', 'Copacabana', '456', 'Avenida B'),
-       (3, 'Belo Horizonte', 'MG', 'Savassi', '789', 'Rua C'),
-       (4, 'Porto Alegre', 'RS', 'Moinhos de Vento', '1011', 'Avenida D'),
-       (5, 'Salvador', 'BA', 'Barra', '1213', 'Rua E'),
-       (5, 'Curitiba', 'PR', 'Batel', '1415', 'Avenida F'),
-       (5, 'Recife', 'PE', 'Boa Viagem', '1617', 'Rua G'),
-       (8, 'Fortaleza', 'CE', 'Meireles', '1819', 'Avenida H'),
-       (9, 'Brasília', 'DF', 'Asa Sul', '2021', 'Rua I'),
-       (10, 'Manaus', 'AM', 'Ponta Negra', '2223', 'Avenida J');
-       
+       (2, 'Rio de Janeiro', 'RJ', 'Copacabana', '456', 'Avenida B'),
+       (3, 'São Paulo', 'SP', 'Vila Madalena', '789', 'Rua C'),
+       (4, 'Belo Horizonte', 'MG', 'Savassi', '456', 'Avenida D'),
+       (5, 'Porto Alegre', 'RS', 'Moinhos de Vento', '789', 'Rua E'),
+       (1, 'São Paulo', 'SP', 'Centro', '123', 'Rua A'),
+       (2, 'Rio de Janeiro', 'RJ', 'Copacabana', '456', 'Avenida B'),
+       (3, 'São Paulo', 'SP', 'Vila Madalena', '789', 'Rua C'),
+       (4, 'Belo Horizonte', 'MG', 'Savassi', '456', 'Avenida D'),
+       (5, 'Porto Alegre', 'RS', 'Moinhos de Vento', '789', 'Rua E'),
+       (1, 'São Paulo', 'SP', 'Centro', '123', 'Rua A'),
+       (2, 'Rio de Janeiro', 'RJ', 'Copacabana', '456', 'Avenida B'),
+       (3, 'São Paulo', 'SP', 'Vila Madalena', '789', 'Rua C'),
+       (4, 'Belo Horizonte', 'MG', 'Savassi', '456', 'Avenida D'),
+       (5, 'Porto Alegre', 'RS', 'Moinhos de Vento', '789', 'Rua E'),
+       (1, 'São Paulo', 'SP', 'Centro', '123', 'Rua A'),
+       (2, 'Rio de Janeiro', 'RJ', 'Copacabana', '456', 'Avenida B'),
+       (3, 'São Paulo', 'SP', 'Vila Madalena', '789', 'Rua C'),
+       (4, 'Belo Horizonte', 'MG', 'Savassi', '456', 'Avenida D'),
+       (5, 'Porto Alegre', 'RS', 'Moinhos de Vento', '789', 'Rua E');
